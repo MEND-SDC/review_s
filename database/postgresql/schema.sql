f67a6967bc3fbf64e8195a95437889f7fda213f3
Derrick Marshall <marshallderrickdnm@gmail.com>
2019-12-20 23:02:27 -0800

    seeding scirpt files

--- database/postgresql/schema.sql
+++ database/postgresql/schema.sql
@@ -1,109 +1,54 @@
-SCHEMA: accounts
+DROP DATABASE IF EXISTS airreviews;
 
-DROP SCHEMA accounts ;
+CREATE DATABASE airreviews
 
-CREATE SCHEMA accounts
-    AUTHORIZATION postgres;
+\c "airreviews";
 
-    CREATE TABLE accounts.user_info
-(
-    user_id bigint NOT NULL DEFAULT nextval('accounts.personal_info_info_id_seq'::regclass),
-    legal_name character varying COLLATE pg_catalog."default" NOT NULL,
-    gender character varying COLLATE pg_catalog."default",
-    "DOB" date NOT NULL,
+CREATE TABLE user_info(
+    id SERIAL PRIMARY KEY,
+    legal_name character NOT NULL,
+    gender character,
+    date_of_birth date,
     email_address date NOT NULL,
     phone_number integer NOT NULL,
     emergency_contact integer,
     join_date date NOT NULL,
-    image_url character varying COLLATE pg_catalog."default",
+    image_url character,
     host boolean NOT NULL,
-    address character varying COLLATE pg_catalog."default",
-    CONSTRAINT personal_info_pkey PRIMARY KEY (user_id)
-)
+    user_address character,
+);
 
-TABLESPACE pg_default;
-
-ALTER TABLE accounts.user_info
-    OWNER to postgres;
-
--- Table: accounts.reviews
-
--- DROP TABLE accounts.reviews;
-
-CREATE TABLE accounts.reviews
-(
-    reviews_id integer NOT NULL DEFAULT nextval('accounts.reviews_reviews_id_seq'::regclass),
+CREATE TABLE reviews(
+    reviews_id INTEGER NOT NULL,
+    user_id INTEGER REFERENCES user_info(id),
     post_date date NOT NULL,
     host_response_date date,
-    host_response character varying COLLATE pg_catalog."default",
+    host_response character,
     check_in_rating real,
     accuracy_rating real,
     value_rating real,
-    "communication_rating " real,
+    communication_rating real,
     location_rating real,
     clean_rating real,
-    CONSTRAINT reviews_pkey PRIMARY KEY (reviews_id),
-    CONSTRAINT user_id FOREIGN KEY (reviews_id)
-        REFERENCES accounts.user_info (user_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
+    PRIMARY KEY (reviews_id, user_id)
+);
 
-TABLESPACE pg_default;
+CREATE TABLE locations(
+    loc_id INTEGER NOT NULL,
+    user_id INTEGER REFERENCES user_info(id),
+    rating_id INTEGER REFERENCES rating(rating_id),
+    title character,
+    loc_address character,
+    PRIMARY KEY (loc_id, user_id, rating_id),
+);
 
-ALTER TABLE accounts.reviews
-    OWNER to postgres;
-
--- Table: accounts.location
-
--- DROP TABLE accounts.location;
-
-CREATE TABLE accounts.location
-(
-    location_id integer NOT NULL,
-    title character varying COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT location_pkey PRIMARY KEY (location_id),
-    CONSTRAINT rating_id FOREIGN KEY (location_id)
-        REFERENCES accounts.rating (rating_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    CONSTRAINT user_id FOREIGN KEY (location_id)
-        REFERENCES accounts.user_info (user_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE accounts.location
-    OWNER to postgres;
-
--- Table: accounts.rating
-
--- DROP TABLE accounts.rating;
-
-CREATE TABLE accounts.rating
-(
-    rating_id integer NOT NULL,
+CREATE TABLE rating(
+    rating_id SERIAL PRIMARY KEY,
     rating_avg real NOT NULL,
     checking_avg real NOT NULL,
     accuracy_avg real NOT NULL,
     value_avg real NOT NULL,
-    "communication_avg " real NOT NULL,
+    communication_avg real NOT NULL,
     location_avg real NOT NULL,
     cleanliness_avg real NOT NULL,
-    CONSTRAINT rating_pkey PRIMARY KEY (rating_id),
-    CONSTRAINT location_id FOREIGN KEY (rating_id)
-        REFERENCES accounts.location (location_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
 )
-
-TABLESPACE pg_default;
-
-ALTER TABLE accounts.rating
-    OWNER to postgres;
